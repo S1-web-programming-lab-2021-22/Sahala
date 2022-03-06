9476a64d80727e9b3fa08c793a349ee934a76e57
sahalabakker <94426450+sahalabakker@users.noreply.github.com>
2022-03-06 22:01:26 +0530

    php

---
+++ asort.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+$students = array("Anu","Manu","Anandhu","Aju");
+echo'Asort';
+echo'<br>';
+asort($students);
+print_r($students);
+echo '<br>';
+echo 'Arsort';
+echo '<br>';
+arsort($students);
+print_r($students);
+?>
+</body>
+</html>
